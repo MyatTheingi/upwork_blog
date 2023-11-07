124ead479c4a98c735c68ea9f6dc16961c9d935d
MyatTheingi <myattheingi555@gmail.com>
2023-11-07 17:34:25 +0630

    [Add] Laravel Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Product\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,24 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ArticleController::class, 'index']);
 
-Route::get("/articles", function () {
-    return "Articles";
-});
+Route::get('/articles', [ArticleController::class, 'index']);
 
-Route::get("/articles/detail", function () {
-    return "Article Detail";
-})->name("article.detail");
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
 
-Route::get("/articles/more", function () {
-    return redirect()->route("article.detail");
-});
+Route::get("/products", [ProductController::class, "index"]);
 
-Route::get("/articles/detail/{id}", function ($id) {
-    return "Article Detail - $id";
-});
-
-// Remark: Resource Route, View Route, Route Group 
+//Remark : Single Action Controller , Resource Controller 

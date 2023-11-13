16d6fe3da1ec07938e0e8040b59c51c4a8577b24
SanKK <145795695+HsanKywal@users.noreply.github.com>
2023-11-13 20:49:10 +0630

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,35 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Article;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
-
+use App\Models\Article;
+use App\Models\Category;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index','detail']);
+    }
     public function index()
     {
         $data = Article::latest()->paginate(5);
-        return view("articles.index", ["articles" => $data]);
+
+        return view('articles.index',[
+            'articles' => $data
+        ]);
     }
 
     public function detail($id)
     {
-        $data = Article::find($id);
-        return view("articles.detail", ["article" => $data]);
+        $article = Article::find($id);
 
+        return view('articles.detail',[
+            'article' => $article
+        ]);
     }
 
-    public function add()
+    public function edit($id)
     {
-        $data = [
-            ["id" => 1, "name" => "News"],
-            ["id" => 2, "name" => "Tech"]
-        ];
-        return view("articles.add", ["categories" => $data]);
+        $article = Article::find($id);
+        $categories = Category::all();
+        return view('articles.edit',[
+            'article' => $article,
+            'categories'=> $categories,
+        ]);
     }
 
-    public function create()
+    public function update($id)
     {
         $validator = validator(request()->all(), [
             "title" => "required",
@@ -42,22 +53,56 @@ class ArticleController extends Controller
             return back()->withErrors($validator);
         }
 
-        $article = new Article();
+        $article = Article::find($id);
         $article->title = request()->title;
         $article->body = request()->body;
         $article->category_id = request()->category_id;
-
+        $article->user_id = auth()->user()->id;
         $article->save();
-        return redirect("/articles");
+
+        return redirect("/articles/detail/$id")->with('success','Successfully edit an article!');
+
+        // return redirect('/articles/details/'+$id)->with('info', 'Edited an article');
+
+    }
+    public function add()
+    {
+        $categories = Category::all();
+        return view('articles.add', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function create()
+    {
+
+        $validator = validator(request()->all(), [
+            "title" => "required",
+            "body" => "required",
+            "category_id" => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        $article = new Article;
+        $article->title = request()->title;
+        $article->body = request()->body;
+        $article->category_id = request()->category_id;
+        $article->save();
+
+        return redirect('/articles');
     }
 
     public function delete($id)
     {
         $article = Article::find($id);
-        $article->delete();
+        if(Gate::allows('delete-article', $article)) {
+            $article->delete();
+            return redirect('/articles')->with('info','Deleted an article');
+        }
 
-        return redirect('/articles')->with('info', 'Article deleted');
-
+        return back()->with('info','Unauthorize');
     }
 }
-
